a7ce76ebee382480d745e15a5551fe3dc58f129b
Sam Burke <74158978+samburke97@users.noreply.github.com>
2025-11-21 09:33:10 +1000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       />
       <ExperienceSection />
       <PartnersCarousel />
-      <ProjectShowcase />
       <ServicesGrid />
+      <ProjectShowcase />
     </div>
   );
 }
