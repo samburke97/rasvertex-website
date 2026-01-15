eb934d33bd391a6d881c6568eb4b9122b0f4c903
Sam Burke <74158978+samburke97@users.noreply.github.com>
2026-01-15 11:33:33 +1000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,18 @@ import ExperienceSection from "./components/homepage/ExperienceSection";
 import PartnersCarousel from "./components/homepage/PartnersCarousel";
 import { mainPanels } from "./data/expandingPanelsData";
 import Hero from "./components/homepage/Hero";
-import ServicesAlternating from "./components/homepage/Hero";
 import Testimonials from "./components/homepage/Testimonials";
 import CTA from "./components/homepage/CTA";
 
 export default function Home() {
   return (
     <>
-      <Hero />
+      <Container>
+        <Hero />
+      </Container>
       <section className="py-[200px]">
         <PartnersCarousel />
       </section>
-
       <Container>
         <section className="pb-[200px]">
           <ExperienceSection />
